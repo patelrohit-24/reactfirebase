f8827bf5190e16e4ebb020bf8cb907e0f2c05f8f
patelrohit-24 <patelrohit797437@gmail.com>
2024-07-01 17:51:29 +0530

    Update Amazon.js

--- src/Amazon.js
+++ src/Amazon.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Amazon() {
     return (
-        <div>Amazon</div>
+        <>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, pariatur libero eum quae labore possimus ea accusantium voluptatibus repudiandae veritatis fugiat fugit porro totam, architecto perferendis veniam autem. Velit, excepturi?</p>
+        </>
     )
 }
